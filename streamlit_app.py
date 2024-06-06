43c6d6e332bac49b71d67b5634d91d318f316a71
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-06-06 16:22:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 # Configuração da página do Streamlit
